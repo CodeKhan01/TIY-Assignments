fd33dcf7c96621dae0f4135cb1fa20c03d002190
Isaiah Khan <ikhanz7@icloud.com>
2015-10-07 12:19:27 -0400

    created Xcode project

---
+++ mission-briefing/mission-briefing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mission-briefing
+//
+//  Created by Isaiah Khan on 10/7/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

2114bd2e5e181131e82e3b60b16ff002c0877806
Isaiah Khan <ikhanz7@icloud.com>
2015-10-13 15:17:34 -0400

    initial commit for hw

---
+++ Jackpot/Jackpot/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Jackpot
+//
+//  Created by Isaiah Khan on 10/13/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+/* 1. Delete ViewController, and create a "Table View Controller" as the main interface so that the user can see.
+    2. Create a Navigation Controller that will handle the user view transitions.
+    3. Add a "button" from the object library and name it "add".
+    4. When you click the "add button", make it generate a random number between 1-53.
+    5. Once it generates 6 random numbers, place it in a cell in the Table View Controller that we created earlier.
+    5. Work on Essay and create a pull request.
+
+*/
